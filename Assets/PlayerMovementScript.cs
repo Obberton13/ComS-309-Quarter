2de34c3360a5704892afdb177dfee2818b9096d1
Ryan Krause <say892@aol.com>
2015-09-23 12:22:42 -0500

    VR and Movement
    Imported the Oculus Library and made a basic script for player movement.

---
+++ Assets/PlayerMovementScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementScript : MonoBehaviour {
+
+	[SerializeField]
+	private float speed = 6.0F;
+
+	[SerializeField]
+	private float jump_force = 8.0F;
+
+	[SerializeField]
+	private float gravity = 20.0F;
+
+	private CharacterController CharControl;
+	private Vector3 moveDir;
+
+	// Use this for initialization
+	void Start () {
+		CharControl = GetComponent<CharacterController>();
+		moveDir = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+
+		if (CharControl.isGrounded) {
+			moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			moveDir = transform.TransformDirection(moveDir);
+			moveDir *= speed;
+
+			if (Input.GetButton("Jump")) {
+				moveDir.y = jump_force;
+			}
+		}
+
+		moveDir.y -= gravity * Time.deltaTime;
+		CharControl.Move(moveDir * Time.deltaTime);
+
+
+
+		
+	}
+}
