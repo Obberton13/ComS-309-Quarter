d828720c7ca3f617f0e269fdc5d6605642aa3c08
Robert <obberton130@gmail.com>
2015-09-26 19:59:49 -0500

    Basic chunking code. Might be easier to instantiate at position.

---
+++ Assets/Scripts/WorldGeneration/Chunk.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chunk : MonoBehaviour {
+
+    public const int chunkSize = 20;
+    [SerializeField]
+    private GameObject _block;
+    private int chunkX, chunkZ;
+
+    void Awake()
+    {
+        Random.seed = 39;
+        Noise.init();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(generateBlocks());
+    }
+
+    private IEnumerator generateBlocks()
+    {
+        for (int x = 0; x < chunkSize; x++)
+        {
+            for (int z = 0; z < chunkSize; z++)
+            {
+                for (int y = 0; y < chunkSize; y++)
+                {
+                    int newX = x + chunkSize * chunkX;
+                    int newY = y;
+                    int newZ = z + chunkSize * chunkZ;
+                    float noise = Noise.getNoiseValue(newX / 8f, newY / 8f, newZ / 8f);
+                    noise *= ((float)newY) / chunkSize;
+                    if (noise < .05f)
+                    {
+                        GameObject.Instantiate(_block, new Vector3(newX, newY, newZ), Quaternion.identity);
+                    }
+                }
+                yield return 0;
+            }
+        }
+    }
+
+    public void setChunkPos(int x, int z)
+    {
+        this.chunkX = x;
+        this.chunkZ = z;
+    }
+
+    public void setPrefab(GameObject prefab)
+    {
+        this._block = prefab;
+    }
+}
--- Assets/Scripts/WorldGeneration/World.cs
+++ Assets/Scripts/WorldGeneration/World.cs
@@ -2,41 +2,16 @@
 using System.Collections;
 
 public class World : MonoBehaviour {
+    public GameObject prefab;
 
-    [SerializeField]
-    private GameObject _block;
-
-    void Awake()
+    void Start()
     {
-        Random.seed = 1;
-        Noise.init();
-    }
-
-	// Use this for initialization
-	void Start () {
-        StartCoroutine(generateBlocks());
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-    private IEnumerator generateBlocks()
-    {
-        for(int x = -10; x < 10; x++)
+        for(int x = -2; x < 3; x++)
         {
-            for(int y = -10; y < 10; y++)
+            for(int z = -2; z < 3; z++)
             {
-                for(int z = -10; z < 10; z++)
-                {
-                    float noise = Noise.getNoiseValue(x / 6f, y / 6f, z / 6f);
-                    if (noise < .4f)
-                    {
-                        GameObject.Instantiate(_block, new Vector3(x, y, z), Quaternion.identity);
-                        yield return 0;
-                    }
-                }
+                Chunk chunk = GameObject.Instantiate(prefab).GetComponent<Chunk>();
+                chunk.setChunkPos(x, z);
             }
         }
     }
