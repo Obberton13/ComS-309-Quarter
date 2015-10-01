78cd0f1e224dab7bb48ac1ddb6d96f686eb4595c
Robert <obberton130@gmail.com>
2015-10-01 13:12:39 -0500

    More work on terrain generation, started building meshes.

--- Assets/Scripts/WorldGeneration/Chunk.cs
+++ Assets/Scripts/WorldGeneration/Chunk.cs
@@ -1,25 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshCollider))]
+[RequireComponent(typeof(MeshFilter))]
 public class Chunk : MonoBehaviour {
     
     [SerializeField]
     private GameObject _block;
-    private int chunkX, chunkZ;
 
-    void Awake()
-    {
-        Random.seed = 39;
-        Noise.init();
-    }
+    private byte[,,] _map = new byte[Constants.chunkWidth, Constants.chunkHeight, Constants.chunkWidth];
 
     // Use this for initialization
     void Start()
     {
-        StartCoroutine(generateBlocks());
+        //System.Threading.Thread thread = new System.Threading.Thread(generateBlocks);
+        //thread.Start();
+        MeshCollider collider = GetComponent<MeshCollider>();
+        MeshRenderer renderer = GetComponent<MeshRenderer>();
+        MeshFilter filter = GetComponent<MeshFilter>();
+        generateBlocks();
+        generateMesh();
     }
 
-    private IEnumerator generateBlocks()
+    private void generateBlocks()
     {
         for (int x = 0; x < Constants.chunkWidth; x++)
         {
@@ -27,26 +32,78 @@ public class Chunk : MonoBehaviour {
             {
                 for (int y = 0; y < Constants.chunkHeight; y++)
                 {
-                    float noise = Noise.getNoiseValue((new Vector3(x, y, z) + transform.position) / 16f);
-                    noise /= (y + 1);
-                    if (noise > .05f)
-                    {
-                        GameObject.Instantiate(_block, new Vector3(x, y, z) + transform.position, Quaternion.identity);
-                    }
+                    _map[x, y, z] = generateBlock(x, y, z);
                 }
-                yield return 0;
             }
         }
     }
 
-    public void setChunkPos(int x, int z)
+    private byte generateBlock(int x, int y, int z)
     {
-        this.chunkX = x;
-        this.chunkZ = z;
+        float noise = Noise.getNoiseValue((new Vector3(x, y, z) + transform.position) / 16f);
+        noise /= (y + 1);
+        if (noise > .04f)
+        {
+            return 1;
+        }
+        return 0;
     }
 
-    public void setPrefab(GameObject prefab)
+    private void generateMesh()
     {
-        this._block = prefab;
+        Mesh mesh = new Mesh();
+
+        List<Vector3> verts = new List<Vector3>();
+        List<Vector2> uvs = new List<Vector2>();
+        List<int> tris = new List<int>();
+
+        for (int x = 0; x < Constants.chunkWidth; x++)
+        {
+            for (int z = 0; z < Constants.chunkWidth; z++)
+            {
+                for(int y = 0; y < Constants.chunkHeight; y++)
+                {
+                    if (_map[x, y, z] == 0)
+                    {
+                        continue;
+                    }
+                    buildBlock(x, y, z, verts, uvs, tris);
+                }
+            }
+        }
+
+        mesh.vertices = verts.ToArray();
+        mesh.uv = uvs.ToArray();
+        mesh.triangles = tris.ToArray();
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+    }
+
+    private void buildBlock(int x, int y, int z, List<Vector3> verts, List<Vector2> uvs, List<int> tris)
+    {
+        Vector3[] corners = { new Vector3(), Vector3.up, Vector3.right, Vector3.up + Vector3.right };
+        buildFace(corners, verts, uvs, tris);
+    }
+
+    private void buildFace(Vector3[] corners, List<Vector3> verts, List<Vector2> uvs, List<int> tris)
+    {
+        int index = verts.Count;
+
+        verts.Add(corners[0]);
+        verts.Add(corners[1]);
+        verts.Add(corners[3]);
+        verts.Add(corners[2]);
+
+        uvs.Add(new Vector2());
+        uvs.Add(Vector2.up);
+        uvs.Add(Vector2.up + Vector2.right);
+        uvs.Add(Vector2.right);
+
+        tris.Add(index);
+        tris.Add(index+1);
+        tris.Add(index+2);
+        tris.Add(index);
+        tris.Add(index+2);
+        tris.Add(index+3);
     }
 }
--- Assets/Scripts/WorldGeneration/World.cs
+++ Assets/Scripts/WorldGeneration/World.cs
@@ -2,16 +2,44 @@
 using System.Collections;
 
 public class World : MonoBehaviour {
-    public GameObject prefab;
+
+    [SerializeField]
+    private GameObject _prefab;
+
+    private Queue _needsGenerated = new Queue();
+    private Queue _doneGenerating = new Queue();
+
+    
+    void Awake()
+    {
+        Random.seed = 1;
+        Noise.init();
+    }
 
     void Start()
     {
-        for(int x = -2; x < 3; x++)
+        for (int x = -2; x < 3; x++)
         {
-            for(int z = -2; z < 3; z++)
+            for (int z = -2; z < 3; z++)
             {
-                GameObject.Instantiate(prefab, new Vector3(Constants.chunkWidth * x, 0, Constants.chunkWidth * z), Quaternion.identity);
+                GameObject.Instantiate(_prefab, new Vector3(Constants.chunkWidth * x, 0, Constants.chunkWidth * z), Quaternion.identity);
+                //TODO instead of instantiate, generate all the vertices and tris.
+                //yield return 0;
             }
+        }
+    }
+
+    void Update()
+    {
+        createChunks();
+    }
+
+    private IEnumerator createChunks()
+    {
+
+        while (true)
+        {
+            yield return 0;
         }
     }
 }
