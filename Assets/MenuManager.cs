45af2cbbc7bed33326166647989415a7aac25c91
Zachary <zkoehn@iastate.edu>
2015-10-07 17:50:51 -0500

    This is the start of the Main menu. Placed buttons

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	public enum Buttons{
+		newGame,
+		loadGame,
+		godMode,
+		specMode,
+		options
+	};
+
+	public Buttons highlighted;
+
+	// Update is called once per frame
+	public void highlightSelected (int selected) {
+		Debug.Log("Shit fuck" + selected);
+	}
+	
+	public void makeMenuSelection(int selection){
+		Debug.Log("FROM: makeMenuSelection " + selection);
+	}
+}
---
+++ Assets/changeSelected.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeSelected : MonoBehaviour {
+
+	// Update is called once per frame
+	public void highlightSelected (int selected) {
+		Debug.Log("Shit fuck" + selected);
+	}
+
+	public void makeMenuSelection(int selection){
+		Debug.Log("FROM: makeMenuSelection " + selection);
+	}
+}
