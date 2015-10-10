215eaf1987b008edb5393668fbd3b19253a4636f
Zachary <zkoehn@iastate.edu>
2015-10-10 13:56:28 -0500

    It now highlights the currently selected menu item.

--- Assets/changeSelected.cs
+++ Assets/changeSelected.cs
@@ -3,12 +3,5 @@ using System.Collections;
 
 public class changeSelected : MonoBehaviour {
 
-	// Update is called once per frame
-	public void highlightSelected (int selected) {
-		Debug.Log("Shit fuck" + selected);
-	}
 
-	public void makeMenuSelection(int selection){
-		Debug.Log("FROM: makeMenuSelection " + selection);
-	}
 }
