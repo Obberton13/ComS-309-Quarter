507baf3129323c48381c6dc718dc195a601776e8
Alex Luehm <luehm@me.com>
2015-10-13 22:48:14 -0500

    Working on testing saving and loading

---
+++ Assets/Scripts/test/LoadButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadButton : MonoBehaviour 
+{
+	
+	void OnGUI()
+	{
+		if (GUI.Button (new Rect (10, 150, 100, 30), "Load Game"))
+		{
+			SaveWorld my_save = this.GetComponent<SaveWorld>();
+			GameObject chunkObject = GameObject.Find("chunk");
+			if(chunkObject == null)
+			{
+				return;
+			}
+			
+			my_save.load(chunkObject.GetComponent<Chunk>().getInfo());
+			
+		}
+	}
+}
