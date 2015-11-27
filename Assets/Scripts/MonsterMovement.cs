a61eaf0c4ebd860a70b2bf2e21f0c2a45d7ddccf
Ryan Krause <say892@aol.com>
2015-11-27 00:47:46 -0600

    Monsters are becoming self-aware and can now fight through the environment to get to the player.

--- Assets/Scripts/MonsterMovement.cs
+++ Assets/Scripts/MonsterMovement.cs
@@ -10,23 +10,28 @@ public class MonsterMovement : MonoBehaviour {
 	private float canHit; //if canHit >= MONSTER_ATTACK_SPEED, the monster can attack.
 	
 	[SerializeField]
-	private float jumpForce = 8.0F;
+	private float jumpForce = 3.0F;
 	
 	[SerializeField]
 	private float gravity = 20.0F;
 	
 	private Vector3 moveDirection = Vector3.zero;
 	private CharacterController controller;
+
+
+	private RaycastHit monsterHit; //information on what the monster is hitting.
+
+
+
+
 	// Use this for initialization
 	void Start () {
-		
 		canHit = MONSTER_ATTACK_SPEED;
 		controller = GetComponent<CharacterController>();
-		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () {  
 		
 		GameObject target = findClosestPlayer();
 		
@@ -37,22 +42,132 @@ public class MonsterMovement : MonoBehaviour {
 			//keep eye contact to know that you mean business.
 			transform.rotation = Quaternion.LookRotation(target.transform.position, Vector3.up);
 			
-			
-
+			//move forward by default
 			if (controller.isGrounded) {
 				moveDirection = (target.transform.position - this.transform.position).normalized; //move towards them. You know you want to.
 				moveDirection.y = 0;
 				moveDirection *= MONSTER_MOVE_SPEED;
 			}
 			
-			
-			if (Physics.CheckSphere(this.transform.position + 1.0F * transform.forward.normalized, 0.49F)) {
-				//There's a block in our way at our feet, so let's jump over it. 
-				moveDirection.y = jumpForce; //TODO experiment with jumpForce to make it reasonable/good.
+
+			//checks if there is a block directly in front of the monster at their feet
+			if (Physics.CheckSphere((this.transform.position - new Vector3(0, .5F, 0)) + 1.01F * transform.forward.normalized, 0.49F)) {
+
+
+				//checks if there is a block directly in front of the monster at their head
+				if (Physics.CheckSphere((this.transform.position + new Vector3(0, .5F, 0)) + 1.01F * transform.forward.normalized, 0.49F)) {
+
+
+					//now there are all 3 blocks in front of the monster! 
+					if (Physics.CheckSphere((this.transform.position + new Vector3(0, 1.5F, 0)) + 1.01F * transform.forward.normalized, 0.49F)) {
+						//case 3
+						//delete cube above head this.transform.position + new Vector3(0, 1F, 0)) + 1.01F * transform.forward.normalized)
+						print("3");
+						if (Physics.Raycast(transform.position + new Vector3(0, 1.5F, 0), transform.forward, out monsterHit, 1.01F)) {
+							if (!monsterHit.transform.GetComponent<MeshCollider>())
+							{
+								if (canHit >= MONSTER_ATTACK_SPEED) {
+									attackBlock(monsterHit.transform.gameObject);
+									canHit = 0;
+								}
+							}
+						}
+					
+					}
+
+					else {
+						//case 2 on your cheat sheet ryan
+						//break the cube at the head (this.transform.position + new Vector3(0, .5F, 0)) + 1.01F * transform.forward.normalized)
+						print("2");
+						if (Physics.Raycast(transform.position + new Vector3(0, 0.5F, 0), transform.forward, out monsterHit, 1.01F)) {
+							if (!monsterHit.transform.GetComponent<MeshCollider>())
+							{
+								if (canHit >= MONSTER_ATTACK_SPEED) {
+									attackBlock(monsterHit.transform.gameObject);
+									canHit = 0;
+								}
+							}
+						}
+					}
+
+
+
+
+				}
+				else {
+
+					//there is a block at feet and above head, so not enough room to jump through to.
+					if (Physics.CheckSphere((this.transform.position + new Vector3(0, 1.5F, 0)) + 1.01F * transform.forward.normalized, 0.49F)) {
+						//case 4 ryan
+						// break the block at (this.transform.position + new Vector3(0, 1F, 0)) + 1.01F * transform.forward.normalized)
+						print("4");
+						if (Physics.Raycast(transform.position + new Vector3(0, 1.5F, 0), transform.forward, out monsterHit, 1.01F)) {
+							if (!monsterHit.transform.GetComponent<MeshCollider>())
+							{
+								if (canHit >= MONSTER_ATTACK_SPEED) {
+									attackBlock(monsterHit.transform.gameObject);
+									canHit = 0;
+								}
+							}
+						}
+					}
+
+					else {
+						print("1");
+						moveDirection.y = jumpForce;
+					}
+				}
+				//There's only a block in our way at our feet, so let's jump over it. 
+				//moveDirection.y = jumpForce;
 				
 				//TODO use Physics.checkCapsule to see if we can fit into the area, if not we need to start breaking blocks.
 				
 			}
+
+			//there is a block at head level and above head level (not feet level)
+			else if (Physics.CheckSphere((this.transform.position + new Vector3(0, .5F, 0)) + 1.01F * transform.forward.normalized, 0.49F)) {
+				if (Physics.CheckSphere((this.transform.position + new Vector3(0, 1.5F, 0)) + 1.01F * transform.forward.normalized, 0.49F)) {
+					//case 6
+					//(this.transform.position + new Vector3(0, .5F, 0)) + 1.01F * transform.forward.normalized)
+					print("6");
+					if (Physics.Raycast(transform.position + new Vector3(0, 1.5F, 0), transform.forward, out monsterHit, 1.01F)) {
+						if (!monsterHit.transform.GetComponent<MeshCollider>())
+						{
+							if (canHit >= MONSTER_ATTACK_SPEED) {
+								attackBlock(monsterHit.transform.gameObject);
+								canHit = 0;
+							}
+						}
+					}
+				}
+				else {
+					//case 5
+					print("5");
+					if (Physics.Raycast(transform.position + new Vector3(0, 1.5F, 0), transform.forward, out monsterHit, 1.01F)) {
+						if (!monsterHit.transform.GetComponent<MeshCollider>())	{
+							if (canHit >= MONSTER_ATTACK_SPEED) {
+								attackBlock(monsterHit.transform.gameObject);
+								canHit = 0;
+							}
+						}
+					}
+				}
+			}
+
+//			//block above head level, must kill because tight fit
+//			else if (Physics.CheckSphere((this.transform.position + new Vector3(0, 1.5F, 0)) + 1.01F * transform.forward.normalized, 0.49F)) {
+//				//case 5
+//				print("5");
+//				if (Physics.Raycast(transform.position + new Vector3(0, 1.5F, 0), transform.forward, out monsterHit, 1.01F)) {
+//					if (!monsterHit.transform.GetComponent<MeshCollider>())	{
+//						if (canHit >= MONSTER_ATTACK_SPEED) {
+//							attackBlock(monsterHit.transform.gameObject);
+//							canHit = 0;
+//						}
+//					}
+//				}
+//			}
+
 			
 
 			moveDirection.y -= gravity * Time.deltaTime; //even monsters get gravity.
@@ -102,4 +217,24 @@ public class MonsterMovement : MonoBehaviour {
 		
 		
 	} //end of findClosestPlayer()
+
+	private void attackBlock(GameObject block) {
+
+		//99% chance to destroy right now
+		//TODO change chance based off of block info.
+		if (Random.Range(0.0F, 1.0F) <= .99F) {
+			Destroy(block); //TODO
+			//Remove from Chunk
+		}
+
+
+	}
+
+
+	//Testing for monster collision
+	void OnDrawGizmos() {
+		Gizmos.DrawSphere(this.transform.position - new Vector3(0, .5F, 0) + 1.01F * transform.forward.normalized, 0.49F); //bottom block in front
+		Gizmos.DrawSphere(this.transform.position + new Vector3(0, .5F, 0) + 1.01F * transform.forward.normalized, 0.49F); //bottom block in front
+		Gizmos.DrawSphere(this.transform.position + new Vector3(0, 1.5F, 0) + 1.01F * transform.forward.normalized, 0.49F); //bottom block in front
+	}
 }
