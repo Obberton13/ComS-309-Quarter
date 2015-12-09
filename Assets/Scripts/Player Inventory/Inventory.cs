07fe4dc777dfd184880e12fd8ab9061d701e0f09
Alex Luehm <luehm@me.com>
2015-12-09 00:41:40 -0600

    Zachs things work?

---
+++ Assets/Scripts/Player Inventory/Inventory.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour
+{
+    ItemDatabase database;
+    GameObject inventoryPanel;
+    GameObject slotPanel;
+    // The slot prefab (assigned in the editor)
+    public GameObject inventorySlot;
+    // The item prefab (assigned in the editor)
+    public GameObject inventoryItem;
+
+    public int numSlots;
+    public List<Item> items = new List<Item>();
+    public List<GameObject> slots = new List<GameObject>();
+
+    void Start()
+    {
+        database = GetComponent<ItemDatabase>();
+
+        //numSlots = 30;
+        inventoryPanel = GameObject.Find("Inventory Panel");
+        slotPanel = inventoryPanel.transform.FindChild("Slot Panel").gameObject;
+
+        for(int i = 0; i < numSlots; ++i)
+        {
+            items.Add(new Item());
+            slots.Add(Instantiate(inventorySlot));
+            slots[i].transform.SetParent(slotPanel.transform);
+        }
+        AddItem(0);
+        AddItem(1);
+        AddItem(0);
+        AddItem(1);
+    }
+
+    public void AddItem(int id)
+    {
+        Item adding = database.FetchItemByID(id);
+
+        int check = checkInInventory(adding);
+        if (check > -1)
+        {
+            ItemData data = slots[check].transform.GetChild(0).GetComponent<ItemData>();
+            data.amount++;
+            data.transform.GetChild(0).GetComponent<Text>().text = data.amount.ToString();
+        }
+        else
+        {
+            for (int i = 0; i < items.Count; ++i)
+            {
+                if (items[i].ID == -1)
+                {
+                    items[i] = adding;
+                    GameObject obj = Instantiate(inventoryItem);
+                    obj.transform.SetParent(slots[i].transform);
+                    obj.GetComponent<Image>().sprite = adding.sprite;
+                    obj.transform.position = Vector2.zero;
+                    //obj.name = adding.Color + " Block";
+                    slots[i].name = adding.Color + " Block";
+                    break;
+                }
+            }
+        }
+    }
+
+    public void RemoveItem(int slot)
+    {
+        items[slot] = new Item();
+    }
+
+    // Returns slot that item current exists in
+    int checkInInventory(Item item)
+    {
+        for (int i = 0; i < items.Count; ++i)
+        {
+            if (items[i].ID == item.ID)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ Assets/Scripts/Player Inventory/ItemDatabase.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using LitJson;
+using System.Collections.Generic;
+using System.IO;
+
+public class ItemDatabase : MonoBehaviour
+{
+    private List<Item> database = new List<Item>();
+    private JsonData itemData;
+
+    void Awake()
+    {
+        // StreamableAssets makes it so that we don't have to recompile the game every time we change the database
+        itemData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/StreamingAssets/blocks.json"));
+        ConstructBlockDatabase();
+    }
+
+    void ConstructBlockDatabase()
+    {
+        for (int i = 0; i < itemData.Count; ++i)
+        {
+            database.Add(new Item((int)itemData[i]["id"], itemData[i]["color"].ToString()));
+        }
+    }
+
+    public Item FetchItemByID(int id)
+    {
+        for (int i = 0; i < database.Count; ++i)
+        {
+            if (database[i].ID == id)
+                return database[i];
+        }
+        return null;
+    }
+}
+
+public class Item
+{
+    public int ID { get; set; }
+    public string Color { get; set; }
+    public Sprite sprite { get; set; }
+
+    public Item()
+    {
+        ID = -1;
+        //Color = "air";
+    }
+
+    public Item(int id, string color)
+    {
+        this.ID = id;
+        this.Color = color;
+        Debug.Log(this.Color + "_block");
+        this.sprite = Resources.Load<Sprite>("Sprites/Items/" + this.Color + "_block");
+        Debug.Log(this.sprite);
+    }
+}
